aa0a8d0943fe6b5ab31609723b8ee36a81608d92
Andrej Zacharevicz <andrej@zahar.ws>
2023-01-15 23:58:48 +0100

    Дадаў black у мэйкфайл каб можна было праверыць стан коду і паглядзець што не так (калі што не так). Абнавіў прататып бібліятэкі з дапамогай black

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ venv:
 test:
 	python3 -m pytest tests
 	
+black:
+	python3 -m black --check tests/ latynkatar.py || python3 -m black --diff tests/ latynkatar.py
--- latynkatar.py
+++ latynkatar.py
@@ -1,4 +1,3 @@
-
 class Cyr2Lat:
     @classmethod
     def convert(cls, text: str) -> str:
@@ -14,39 +13,39 @@ class Cyr2Lat:
                     pass
             # Галосныя: А, Е, Ё, У, О, Ю, Э, Я, І
             elif text[index].lower() == "а":
-                converted_text += "a" if  text[index].islower() else "A"
+                converted_text += "a" if text[index].islower() else "A"
             elif text[index].lower() == "э":
-                converted_text += "e" if  text[index].islower() else "E"
+                converted_text += "e" if text[index].islower() else "E"
             elif text[index].lower() == "у":
-                converted_text += "u" if  text[index].islower() else "U"
+                converted_text += "u" if text[index].islower() else "U"
             elif text[index].lower() == "о":
-                converted_text += "o" if  text[index].islower() else "O"
+                converted_text += "o" if text[index].islower() else "O"
             elif text[index].lower() == "і":
-                #TODO: Dadać pierachod u J
-                converted_text += "i" if  text[index].islower() else "I"
+                # TODO: Dadać pierachod u J
+                converted_text += "i" if text[index].islower() else "I"
             elif text[index].lower() == "е":
                 if text[index - 1].lower() == "л":
-                    converted_text += "e" if  text[index].islower() else "E"
+                    converted_text += "e" if text[index].islower() else "E"
                 else:
-                    converted_text += "ie" if  text[index].islower() else "Ie"
-                    #TODO: Dadać pierachod u J
+                    converted_text += "ie" if text[index].islower() else "Ie"
+                    # TODO: Dadać pierachod u J
             elif text[index].lower() == "ё":
                 if text[index - 1].lower() == "л":
-                    converted_text += "o" if  text[index].islower() else "O"
+                    converted_text += "o" if text[index].islower() else "O"
                 else:
-                    converted_text += "io" if  text[index].islower() else "Io"
-                    #TODO: Dadać pierachod u J
+                    converted_text += "io" if text[index].islower() else "Io"
+                    # TODO: Dadać pierachod u J
             elif text[index].lower() == "ю":
                 if text[index - 1].lower() == "л":
-                    converted_text += "u" if  text[index].islower() else "U"
+                    converted_text += "u" if text[index].islower() else "U"
                 else:
-                    converted_text += "iu" if  text[index].islower() else "Iu"
-                    #TODO: Dadać pierachod u J
+                    converted_text += "iu" if text[index].islower() else "Iu"
+                    # TODO: Dadać pierachod u J
             elif text[index].lower() == "я":
                 if text[index - 1].lower() == "л":
-                    converted_text += "a" if  text[index].islower() else "A"
+                    converted_text += "a" if text[index].islower() else "A"
                 else:
-                    converted_text += "ia" if  text[index].islower() else "Ia"
-                    #TODO: Dadać pierachod u J
-                    
+                    converted_text += "ia" if text[index].islower() else "Ia"
+                    # TODO: Dadać pierachod u J
+
         return converted_text
