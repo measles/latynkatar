9803443892d9f4252ea8d51b791df04fef1f0f44
Andrej Zacharevicz <andrej@zahar.ws>
2024-02-04 02:09:29 +0300

    Дадаў тэст на рэальны урывак тэксту (#4)
    Выправіў бібліятэку, бо нехапала часткі літар і былі памылкі ў апрацоўцы
    Дадаў літару ґ пра яку нават і не думаў, бо не карыстаюся
    Абнавіў мэйкфайл каб пры памылцы параўнання выводзіла поўны тэкст, а не скарочаны

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ venv:
 	python3 -m venv venv
 	
 test:
-	python3 -m pytest tests -lv -ra
+	python3 -m pytest tests -lvv -ra
 	
 black:
 	python3 -m black --check tests/ latynkatar.py || python3 -m black --diff tests/ latynkatar.py
---
+++ tests/test_tekst.py
@@ -0,0 +1,30 @@
+import pytest
+from latynkatar import Cyr2Lat
+
+# Узор узяты з часопіса PAMYŁKA: 
+# https://github.com/PAMYLKA-ZIN/pamylka-number-3/tree/main/PAMYLKA_ZIN_3_FOR_SHARING
+PRYKŁAD = """Вітаем цябе, чытачу!
+Гэта трэці нумар PAMYŁKA ZIN!
+Мы вельмі цешымся, што да каманды стваральнікаў працягваюць далучацца
+новыя навукоўцы і мастакі! І мы будзем радыя кожнаму новаму ўдзельніку!
+Сябры, мы рэдакцыяй надумалі запачаткаваць прэмію – «Бізон Гіґс». Таму гэты
+нумар мы прысвячаем усім беларускім навукоўцам і хочам анансаваць прэмію,
+якая будзе ўвасабляць сабой Беларусь і навуку разам! Гэта ўзнагарода для
+беларускіх навукоўцаў і даследнікаў дакладных і прыродазнаўчых навук ад
+навукова-папулярнага часопісу Pamyłka Zin.
+Больш дэталяў апавядае першы артыкул нумару.
+"""
+UZOR = """Vitajem ciabie, čytaču!
+Heta treci numar PAMYŁKA ZIN!
+My vielmi ciešymsia, što da kamandy stvaralnikaǔ praciahvajuć dałučacca
+novyja navukoǔcy i mastaki! J my budziem radyja kožnamu novamu ǔdzielniku!
+Siabry, my redakcyjaй nadumali zapačatkavać premiju – «Bizon Higs». Tamu hety
+numar my prysviačajem usim biełaruskim navukoǔcam i chočam anansavać premiju,
+jakaja budzie ǔvasablać saboй Biełaruś i navuku razam! Heta ǔznaharoda dla
+biełaruskich navukoǔcaǔ i daslednikaǔ dakładnych i pryrodaznaǔčych navuk ad
+navukova-papularnaha časopisu Pamyłka Zin.
+Bolš detalaǔ apaviadaje pieršy artykuł numaru.
+"""
+
+def test_pryład():
+    assert Cyr2Lat.convert(PRYKŁAD) == UZOR
