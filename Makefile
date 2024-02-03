201bc3e2ea78c07c9d08829cc21d524dda00137e
Andrej Zacharevicz <andrej@zahar.ws>
2024-02-03 18:49:40 +0100

    Выправіў код канвертаціі, прыбраў паўторы Дадаў апрацоўку перахода ад j да i у спалучэннях, кшталту je, jo, ju Дадаў другі тэст (на спалучэнні з "Ю") Код мусіць працаваць з поўным кірылічным алфавітам

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ venv:
 	python3 -m venv venv
 	
 test:
-	python3 -m pytest tests
+	python3 -m pytest tests -lv -ra
 	
 black:
 	python3 -m black --check tests/ latynkatar.py || python3 -m black --diff tests/ latynkatar.py
--- tests/test_zyćnyja.py
+++ tests/test_zyćnyja.py
@@ -4,3 +4,7 @@ from latynkatar import Cyr2Lat
 
 def test_l():
     assert Cyr2Lat.convert("ЛаЭлЯЛуЛіЛюЛЁлЕлЬ") == "ŁaElAŁuLiLuLOlEl"
+
+
+def test_ju():
+    assert Cyr2Lat.convert("ЮрліВец лЮбіЦь лІю п'ю") == "JurliViec lUbiĆ lIju pju"
