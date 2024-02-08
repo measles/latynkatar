c1d2ab94f02e63cd3248e61dba454cb981f5db18
Andrej Zacharevicz <andrej@zahar.ws>
2024-02-08 23:55:22 +0300

    Расшырыў апісанне README да мінімальна прымальнага ўзроўню (#13)
    Дадаў заўвагі пра ліцэнзію і аўтарскія правы ў файлы зыходнікаў бібліятэкі

--- src/latynkatar/latynkatar.py
+++ src/latynkatar/latynkatar.py
@@ -1,3 +1,23 @@
+"""
+This file is part of Latynkatar.
+
+Latynkatar is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License v3 (LGPLv3) as published by the 
+Free Software Foundation, either version 3 of the License, or (at your option) 
+any later version.
+
+Latynkatar is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Lesser General Public License v3 (LGPLv3) for 
+more details. In file LICENSE which should came with a package, or look at it
+in the repo, see <https://github.com/measles/latynkatar/blob/main/LICENSE>.
+
+You should have received a copy of the GNU Lesser General Public License v3 
+(LGPLv3) along with Latynkatar. If not, see <https://www.gnu.org/licenses/>. 
+
+:copyright: (c) 2023, 2024 by Andrej Zacharevicz.
+"""
+
 from .variables import (
     PRAVILY_KANVERTACYJ,
     HALOSNYJA,
