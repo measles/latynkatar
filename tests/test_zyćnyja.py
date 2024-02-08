d0564832ab3f54d20eada58a62ab45398215d45d
Andrej Zacharevicz <andrej@zahar.ws>
2024-02-08 14:40:40 +0100

    Дадаў тэст тэксту з вершам Максіма Багдановіча Зрабіў магчымым запускаць тэсты з усталяванага модуля гэтак жа, які і лакальна Памяняў мэйкфайл:  - Усталёўка залежнасцяў у асяроддзе  - Тэст пакету у асобным асяроддзі test-venv і стварэнне гэтага асяроддзя з усталёўкай патрэбных пакетаў  - Зборка/запампоўка пакету ў сваім асобным асяроддзі і стварэнне асяроддзя з усталёўкай пакетаў, калі асяроддзя няма

--- tests/test_zyćnyja.py
+++ tests/test_zyćnyja.py
@@ -1,5 +1,8 @@
-import pytest
-from src.latynkatar import Cyr2Lat
+# Try to import from module, else import from the source code
+try:
+    from latynkatar import Cyr2Lat
+except ModuleNotFoundError:
+    from src.latynkatar import Cyr2Lat
 
 
 def test_l():
