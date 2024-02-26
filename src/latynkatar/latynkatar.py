85bc97ab64ed09064d32f342f9af56db9f451636
Andrej Zacharevicz <andrej@zahar.ws>
2024-02-26 09:04:10 +0100

    Выдаліў лішні прабел ва ўмовах канвертацыі літар на J

--- src/latynkatar/latynkatar.py
+++ src/latynkatar/latynkatar.py
@@ -74,7 +74,7 @@ class Cyr2Lat:
                     or previous_letter.lower() in HALOSNYJA
                     or not previous_letter.isalpha()
                     or previous_letter == "'"
-                    or previous_letter.lower() ==  "ь"
+                    or previous_letter.lower() == "ь"
                 ) and current_letter.lower() != "і":
                     base = "j" if current_letter.islower() else "J"
                 else:
