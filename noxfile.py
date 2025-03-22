cc427a50c5435246886fda60e3012db7c47e072f
Andrej Zacharevicz <andrej@zahar.ws>
2025-03-22 19:23:49 +0300

    Replace Makefile by tox (#46)
    * Replace Makefile by tox
    * Add flake8 using nox

---
+++ noxfile.py
@@ -0,0 +1,42 @@
+import nox
+
+nox.options.sessions = ["black", "flake8", "test"]
+
+
+@nox.session(tags=("tests", "lint"))
+def black(session):
+    session.install("black")
+    session.run("black", "--check", "tests/", "src/", "noxfile.py")
+
+
+@nox.session
+def black_diff(session):
+    session.install("black")
+    session.run("black", "--diff", "tests/", "src/", "noxfile.py")
+
+
+@nox.session
+def blacked(session):
+    session.install("black")
+    session.run("black", "tests", "src", "noxfile.py")
+
+
+@nox.session(tags=("tests", "lint"))
+def flake8(session):
+    session.install("flake8")
+    session.run("flake8", ".", "--count", "--exclude", ".nox,venv")
+
+
+@nox.session(tags=("tests"))
+def test(session):
+    session.install("pytest", "pytest-html")
+    session.run(
+        "python3",
+        "-m",
+        "pytest",
+        "tests",
+        "-lvv",
+        "-ra",
+        "--html=report.html",
+        "--self-contained-html",
+    )
--- src/latynkatar/__init__.py
+++ src/latynkatar/__init__.py
@@ -2,20 +2,20 @@
 This file is part of Łatynkatar.
 
 Latynkatar is free software: you can redistribute it and/or modify it under the
-terms of the GNU Lesser General Public License v3 (LGPLv3) as published by the 
-Free Software Foundation, either version 3 of the License, or (at your option) 
+terms of the GNU Lesser General Public License v3 (LGPLv3) as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
 any later version.
 
-Łatynkatar is distributed in the hope that it will be useful, but WITHOUT ANY 
+Łatynkatar is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU Lesser General Public License v3 (LGPLv3) for 
+PARTICULAR PURPOSE. See the GNU Lesser General Public License v3 (LGPLv3) for
 more details. In file LICENSE which should came with a package, or look at it
 in the repo, see <https://github.com/measles/latynkatar/blob/main/LICENSE>.
 
-You should have received a copy of the GNU Lesser General Public License v3 
-(LGPLv3) along with Łatynkatar. If not, see <https://www.gnu.org/licenses/>. 
+You should have received a copy of the GNU Lesser General Public License v3
+(LGPLv3) along with Łatynkatar. If not, see <https://www.gnu.org/licenses/>.
 
 :copyright: (c) 2023, 2024 by Andrej Zacharevicz.
 """
 
-from .latynkatar import Cyr2Lat
+from .latynkatar import Cyr2Lat  # noqa: F401
